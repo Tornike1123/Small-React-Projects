8063515cb6b3ae17fa245fe9083c8216c88e83e0
Tornike1123 <toko36106@gmail.com>
2022-11-10 18:53:39 +0400

    Destruction props - 2.destruction in body

--- class-component-state/src/welcome/Greet.js
+++ class-component-state/src/welcome/Greet.js
@@ -1,6 +1,7 @@
 import React from "react";
 
-const Greet = ({ name, heroName }) => {
+const Greet = (props) => {
+	const { name, heroName } = props;
 	return (
 		<div>
 			My name is {name}, Super Hero {heroName}
