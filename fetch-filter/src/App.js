02d47d2b31193545eefea649392d08f780675cee
Tornike1123 <toko36106@gmail.com>
2023-01-29 00:03:42 +0400

    Finish

--- fetch-filter/src/App.js
+++ fetch-filter/src/App.js
@@ -1,36 +1,10 @@
 import "./App.css";
-import React, { useState, useEffect } from "react";
+import Fetch from "./Fetch.jsx";
 
 function App() {
-	const [data, setData] = useState([]);
-	const [searchTerm, setSearchTerm] = useState("");
-
-	useEffect(() => {
-		fetch("https://jsonplaceholder.typicode.com/todos")
-			.then((response) => response.json())
-			.then((data) => setData(data));
-	}, []);
-
-	const handleSearch = (event) => {
-		setSearchTerm(event.target.value);
-	};
-
-	const filteredData = data.filter((item) =>
-		item.title.toLowerCase().includes(searchTerm.toLowerCase())
-	);
-
 	return (
 		<div>
-			<input
-				type="text"
-				placeholder="Search Todo Items"
-				value={searchTerm}
-				onChange={handleSearch}
-			/>
-			<h2>Todo Items</h2>
-			{filteredData.map((item) => (
-				<p key={item.id}>{item.title}</p>
-			))}
+			<Fetch />
 		</div>
 	);
 }
---
+++ fetch-filter/src/Fetch.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import "./Fetch.css";
+
+const Fetch = () => {
+	const [data, setData] = useState([]);
+	const [searchTerm, setSearchTerm] = useState("");
+
+	useEffect(() => {
+		fetch("https://jsonplaceholder.typicode.com/photos")
+			.then((response) => response.json())
+			.then((data) => setData(data));
+	}, []);
+
+	const handleSearch = (event) => {
+		setSearchTerm(event.target.value);
+	};
+
+	const filteredData = data.filter((item) =>
+		item.title.toLowerCase().includes(searchTerm.toLowerCase())
+	);
+
+	return (
+		<div className="center">
+			<input
+				type="text"
+				placeholder="Search"
+				value={searchTerm}
+				onChange={handleSearch}
+			/>
+			{filteredData.map((item) => (
+				<div>
+					<p className="fl">{item.title}</p>
+					<img src={item.thumbnailUrl} alt="" />
+				</div>
+			))}
+		</div>
+	);
+};
+
+export default Fetch;
